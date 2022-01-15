67ac54913c8927a9e919988c69f76e57f2ca750e
SmoleacMihai <90186391+SmoleacMihai@users.noreply.github.com>
2022-01-15 22:57:50 +0200

    fixed some padding and centered random page contents

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,28 @@
+// Variables
+let usedRandom = 11
+let curentRandom = 0
 
-   let element = document.getElementsByClassName("text-box")
-   element.style.display = "none"
+//Functions
+function getRandomNumberBetween(min,max){
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+function unhider(){
+    let clear = document.getElementById("content0")
+    clear.style.display = "none"
+    curentRandom = getRandomNumberBetween(1,10)
+    while(curentRandom == usedRandom){
+        curentRandom = getRandomNumberBetween(1,10)
+    }
+    element = document.getElementById("content"+curentRandom)
+    element.style.display = "block";
+    usedRandom = curentRandom
+}
+
+ //main program
+for (var i = 0; i < 11; i++) {
+    cleaner = document.getElementById("content"+i)
+    cleaner.style.display = "none";
+ }
+
+ document.getElementById("randomButton").onclick = unhider() 
