45cfdf128bfe1750a2352435d43f2e5759c90d8a
SmoleacMihai <90186391+SmoleacMihai@users.noreply.github.com>
2022-01-16 02:12:39 +0200

    added jquerry and added content - div for first entering on page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,37 @@
-// Variables
-let usedRandom = 11
-let curentRandom = 0
-
-//Functions
 function getRandomNumberBetween(min,max){
-    return Math.floor(Math.random()*(max-min+1)+min);
+   return Math.floor(Math.random()*(max-min+1)+min);
 }
 
-function unhider(){
-    let clear = document.getElementById("content0")
-    clear.style.display = "none"
-    curentRandom = getRandomNumberBetween(1,10)
-    while(curentRandom == usedRandom){
-        curentRandom = getRandomNumberBetween(1,10)
-    }
-    element = document.getElementById("content"+curentRandom)
-    element.style.display = "block";
-    usedRandom = curentRandom
+function clicked(control){
+   if(control == true){
+      $('#content'+usedRandom).removeClass('text-box');
+      $('#content'+usedRandom).addClass('text-box noneDisplay');
+   }
 }
 
- //main program
-for (var i = 0; i < 11; i++) {
-    cleaner = document.getElementById("content"+i)
-    cleaner.style.display = "none";
- }
 
- document.getElementById("randomButton").onclick = unhider() 
+
+// function unhider(){
+//     const curentRandom = getRandomNumberBetween(1,10)
+//     while(curentRandom == usedRandom){
+//         curentRandom = getRandomNumberBetween(1,10)
+//     }
+//     element = document.getElementById("content"+curentRandom)
+//     element.style.display = "block";
+//     usedRandom = curentRandom
+// }
+
+//  //main program
+
+//  document.getElementById("getAdviceBtn").onclick = unhider() 
+
+ $('#getAdviceBtn').click(function() {
+   let controler = false
+   let i = getRandomNumberBetween(1,10)
+   let usedRandom = i
+   while(i == usedRandom){
+           i = getRandomNumberBetween(1,10)
+   }
+   $('#content'+i).removeClass('text-box noneDisplay');
+   $('#content'+i).addClass('text-box');
+});
