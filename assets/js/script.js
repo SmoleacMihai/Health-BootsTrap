f132b0a826766e31bd5cf86118937990ffc37fd6
SmoleacMihai <90186391+SmoleacMihai@users.noreply.github.com>
2022-01-17 11:36:56 +0200

    count operator in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,12 +9,12 @@ function clicked(control){
    }
 }
 
-function firstClick(firstTime){
-   
+function replace(){
+
 }
 
 
-
+let count = 0
 // function unhider(){
 //     const curentRandom = getRandomNumberBetween(1,10)
 //     while(curentRandom == usedRandom){
@@ -29,13 +29,26 @@ function firstClick(firstTime){
 
 //  document.getElementById("getAdviceBtn").onclick = unhider() 
 
- $('#getAdviceBtn').click(function() {
-   let controler = false
+ $('#getAdviceBtn').click(function(){
+   $('#content0').removeClass('text-box');
+   $('#content0').addClass('text-box noneDisplay');
    let i = getRandomNumberBetween(1,10)
    let usedRandom = i
+   let delLater = i
    while(i == usedRandom){
            i = getRandomNumberBetween(1,10)
    }
    $('#content'+i).removeClass('text-box noneDisplay');
    $('#content'+i).addClass('text-box');
+   ++count
+   console.log(count)
+   i = getRandomNumberBetween(1,10)
+   usedRandom = i
+   let delMoreLater = i
+   if(count >= 2){
+      $('#content'+delLater).removeClass('text-box');
+      $('#content'+delLater).addClass('text-box noneDisplay');
+      $('#content'+delMoreLater).removeClass('text-box');
+      $('#content'+delMoreLater).addClass('text-box noneDisplay');
+   }
 });
