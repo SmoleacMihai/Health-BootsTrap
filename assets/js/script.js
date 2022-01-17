a346cbfd56d0ceabd8e6b6ac6a599c309f97d03e
SmoleacMihai <90186391+SmoleacMihai@users.noreply.github.com>
2022-01-17 15:15:55 +0200

    finished website

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,32 +2,7 @@ function getRandomNumberBetween(min,max){
    return Math.floor(Math.random()*(max-min+1)+min);
 }
 
-function clicked(control){
-   if(control == true){
-      $('#content'+usedRandom).removeClass('text-box');
-      $('#content'+usedRandom).addClass('text-box noneDisplay');
-   }
-}
-
-function replace(){
-
-}
-
-
-let count = 0
-// function unhider(){
-//     const curentRandom = getRandomNumberBetween(1,10)
-//     while(curentRandom == usedRandom){
-//         curentRandom = getRandomNumberBetween(1,10)
-//     }
-//     element = document.getElementById("content"+curentRandom)
-//     element.style.display = "block";
-//     usedRandom = curentRandom
-// }
-
-//  //main program
 let i = 0
-//  document.getElementById("getAdviceBtn").onclick = unhider() 
 let usedRandom = 11
  $('#getAdviceBtn').click(function(){
    for(i = 0; i < 11; i++){
